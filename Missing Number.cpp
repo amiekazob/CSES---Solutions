765dc5ff55f7e440bdbde0945441fedfdaf5a007
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2024-01-02 18:43:42 +0600

    Create Missing Number.cpp

---
+++ Missing Number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main() {
+  ios_base::sync_with_stdio(0);cin.tie(0);
+  ll n ; cin >> n;
+  int arr[n-1]; ll sum = 0;
+  for (int i = 0 ; i < n - 1; i++) {
+    cin >> arr[i];
+    sum += arr[i];
+  }
+  ll res = ((n*n) + n)/2;
+  cout << res - sum << "\n";
+  return 0;
+}
+
